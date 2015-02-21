ba8451d36f08f48d196bfcd8defaebd2671e1790
Jimmy <jimmylorunning@gmail.com>
2015-02-21 16:36:15 -0500

    Initial commit

---
+++ csv_geocoder.rb
@@ -0,0 +1,39 @@
+require 'csv'
+require 'geocoder'
+
+class CSVGeocoder
+
+	attr_accessor :csv, :address_label
+
+	def initialize(file)
+		set_csv(file)
+	end
+
+	def set_csv(file)
+		@csv = CSV.read(file)
+	end
+
+	def get_address_index
+		@csv[0].index(@address_label)
+	end
+
+	def get_lat_lng(address)
+		return nil if address.nil?
+		gc = Geocoder.search(address)
+		gc.first.geometry['location'] if gc
+	end
+
+	def get_addresses
+		addr_index = get_address_index
+		@csv.map do |row|
+			row[addr_index]
+		end
+	end
+
+	def get_lat_lngs
+		get_addresses.map do |address|
+			(address == nil) ? nil : (get_lat_lng address)
+		end
+	end
+
+end
